c70cfbb2b2c533e88b2a4a42f6b40671e06bf78d
muskanagarwal00 <161311810+muskanagarwal00@users.noreply.github.com>
2024-04-03 11:01:36 +0530

    Update Chat.tsx

--- frontend/src/pages/chat/Chat.tsx
+++ frontend/src/pages/chat/Chat.tsx
@@ -10,7 +10,7 @@ import { isEmpty } from "lodash-es";
 import DOMPurify from 'dompurify';
 
 import styles from "./Chat.module.css";
-import Contoso from "../../assets/Contoso.svg";
+import Contoso from "../../assets/Screenshot 2024-01-24 135327.png";
 import { XSSAllowTags } from "../../constants/xssAllowTags";
 
 import {
